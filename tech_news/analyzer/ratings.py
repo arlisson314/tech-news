9a7d46d6dc68032de36dbd6b47fa64c9dfb28575
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-25 18:34:52 -0300

    feat: implementa a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,6 @@
 from tech_news.database import find_news
 from operator import itemgetter
+from collections import Counter
 
 
 # Requisito 10
@@ -18,4 +19,9 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    all_categories = [new["category"] for new in news]
+    top_five_list = sorted(
+        Counter(all_categories).most_common(5), key=lambda x: (-x[1], x[0])
+    )
+    return [category[0] for category in top_five_list]
