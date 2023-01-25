30651f731b52e577bc82211f922e0f2d10c6d8c1
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-25 00:10:57 -0300

    feat: implementa a função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    # top_five_list = []
+    news = find_news()
+    news.sort(key=itemgetter("comments_count", "title"), reverse=True)
+
+    if len(news) <= 5:
+        return [(new["title"], new["url"]) for new in news]
+
+    top_five_list = [news[i] for i in range(5)]
+
+    return [(new["title"], new["url"]) for new in top_five_list]
 
 
 # Requisito 11
