01a941bc3a05deaa4a3185710289fe787880579a
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-24 21:45:35 -0300

    feat: implementa a função search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -27,4 +27,5 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    search = search_news({"category": {"$regex": category, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in search]
